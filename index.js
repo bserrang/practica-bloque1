1fa4a54aa1d30f7ad992317d889428e34bd3a588
bserrang <bserrang@everis.com>
2020-10-21 16:11:32 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// CONTENIDO DE LA PRÁCTICA:
+// Vamos a crear una lista de elementos directamente desde javascript a partir del array aportado en este documento, en la variable "colorList"
+
+
+const colorList = [
+  {
+    colorName: 'white',
+    hex: '#ffffff'
+  },
+  {
+    colorName: '',
+    hex: '#'
+  },
+  {
+    colorName: '',
+    hex: ''
+  },
+  {
+    colorName: '',
+    hex: ''
+  },
+  {
+    colorName: '',
+    hex: ''
+  },
+  {
+    colorName: '',
+    hex: ''
+  },
+  {
+    colorName: '',
+    hex: ''
+  }
+]
