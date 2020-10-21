2219c57f21d14c3d11f2033c1c84a15fb6c5f626
bserrang <bserrang@everis.com>
2020-10-21 16:36:43 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 // CONTENIDO DE LA PRÁCTICA:
-// Vamos a crear una lista de elementos directamente desde javascript a partir del array aportado en este documento, en la variable "colorList"
+// Vamos a crear una lista de elementos directamente desde javascript a partir del array aportado en este documento, en la variable "colorList" (ver imagen en el proyecto "ejemplo_lista.png").
 
+// Cada elemento del listado contendrá:
+//    * El nombre del color.
+//    * Una muestra en la que se ve el color.
+//    * Un botón que modifica el color del siguiente elemento de la lista.
+//    * Un botón que modifica el color del fondo de la página.
+// La información de cada item la obtendremos de cada objeto del array "colorList"
+
+// En esta práctica hay que añadir 4 funcionalidades:
+//    * Al hacer click directamente (no a algo que esté por encima) sobre el fondo de la página (elemento body), debe aparecer un alert en el que se indique el color que tiene en ese momento en hexadecimal.
+//    * Al hacer click directamente sobre uno de los items de la lista debe aparecer un alert en el que se indique el color que tiene en ese momento en hexadecimal
+//    * Al hacer click sobre el botón con el texto "Next item color" deberá aplicarse el color de ese item al color de fondo del siguiente item (el último item cambia al primero).
+//    * Al hacer click sobre el botón con el texto "Page color" deberá aplicarse el color de ese item al color de fondo de la página (elemento body).
+
+// Buena suerte!
 
 const colorList = [
   {
@@ -8,27 +22,31 @@ const colorList = [
     hex: '#ffffff'
   },
   {
-    colorName: '',
-    hex: '#'
+    colorName: 'red',
+    hex: '#ff0000'
   },
   {
-    colorName: '',
-    hex: ''
+    colorName: 'orange',
+    hex: '#ffa500'
   },
   {
-    colorName: '',
-    hex: ''
+    colorName: 'yellow',
+    hex: '#ffff00'
   },
   {
-    colorName: '',
-    hex: ''
+    colorName: 'orchid',
+    hex: '#da70d6'
   },
   {
-    colorName: '',
-    hex: ''
+    colorName: 'pink',
+    hex: '#ffc0cb'
   },
   {
-    colorName: '',
-    hex: ''
+    colorName: 'green',
+    hex: '#008000'
+  },
+  {
+    colorName: 'silver',
+    hex: '#c0c0c0'
   }
 ]
